8d022ce5a4d4899c4591c375979dcd584aab8952
Denis Vasilkovskii <denis.vasilkovskii@tado.com>
2020-10-23 20:29:00 +0200

    Forward port: feat(defines): Add 4.01 Unauthorized response support.

--- coapthon/defines.py
+++ coapthon/defines.py
@@ -208,6 +208,7 @@ class Codes(object):
     CONTINUE = CodeItem(95, 'CONTINUE')
 
     BAD_REQUEST = CodeItem(128, 'BAD_REQUEST')
+    UNAUTHORIZED = CodeItem(129, 'UNAUTHORIZED')
     FORBIDDEN = CodeItem(131, 'FORBIDDEN')
     NOT_FOUND = CodeItem(132, 'NOT_FOUND')
     METHOD_NOT_ALLOWED = CodeItem(133, 'METHOD_NOT_ALLOWED')
@@ -239,6 +240,7 @@ class Codes(object):
         95: CONTINUE,
 
         128: BAD_REQUEST,
+        129: UNAUTHORIZED,
         131: FORBIDDEN,
         132: NOT_FOUND,
         133: METHOD_NOT_ALLOWED,
